5d5ab6f2e469d5cf92e5c1e10da67aeba16591f6
Harshit Singla <122436259+harshit391@users.noreply.github.com>
2024-06-10 17:17:06 +0530

    Day1

---
+++ src/Day1/CheckIfArrayEqualOrNot.java
@@ -0,0 +1,61 @@
+
+
+import java.io.*;
+
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0)
+		{
+		  int n=sc.nextInt();
+		  long arr[]=new long[n];
+		  long brr[]=new long[n];
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      arr[i]=sc.nextLong();
+		  }
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      brr[i]=sc.nextLong();
+		  }
+		  Solution ob = new Solution();
+		  System.out.println(ob.check(arr,brr,n)==true?"1":"0");
+		  
+		  
+		}
+	}
+}
+
+class Solution{
+    
+    public static boolean check(long A[],long B[],int N)
+    {
+        Map<Long,Integer> map = new HashMap<>();
+        
+        for (long i : A) {
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+        
+        for (long i : B) {
+            if (!map.containsKey(i)) {
+                return false;
+            }
+            map.put(i, map.get(i)-1);
+        }
+        
+        for (long i : map.keySet()) {
+            if (map.get(i) != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+}
