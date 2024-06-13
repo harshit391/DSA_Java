ab59f71ed60ab14343c0bb7b9df3366a30fb321f
Harshit Singla <harshitsingla1761@gmail.com>
2024-06-13 23:24:02 +0530

    Day4

--- src/Day2/MoveAllNegetiveToTheEnd.java
+++ src/Day2/MoveAllNegetiveToTheEnd.java
@@ -10,13 +10,13 @@ class MoveAllNegetiveToTheEnd {
         int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine().trim());
-            int a[] = new int[(int)(n)];
-            String inputLine[] = br.readLine().trim().split(" ");
+            int[] a = new int[(int)(n)];
+            String[] inputLine = br.readLine().trim().split(" ");
             for (int i = 0; i < n; i++) {
                 a[i] = Integer.parseInt(inputLine[i]);
             }
 
-            SolutionFindFine obj = new SolutionFindFine();
+            Solution obj = new Solution();
             obj.segregateElements(a, n);
 
             for (int i = 0; i < n; i++) ot.print(a[i] + " ");
@@ -29,7 +29,7 @@ class MoveAllNegetiveToTheEnd {
 
 class Solution {
 
-    public void segregateElements(int arr[], int n) {
+    public void segregateElements(int[] arr, int n) {
         int[] ans = new int[n];
 
         int pos = 0;
---
+++ src/Test.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class Test {
+
+    static void swap(long[] nums, int a, int b)
+    {
+        long temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+    static void arrange(long[] nums, int n)
+    {
+        int i = 0;
+
+        while (i<n)
+        {
+            long correct = nums[i];
+
+            if (nums[i] < nums.length && nums[i]!=nums[(int)correct])
+                swap(nums, i, (int)correct);
+            else i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        long[] arr = new long[5];
+
+        arr[0] = 4;
+        arr[1] = 0;
+        arr[2] = 2;
+        arr[3] = 1;
+        arr[4] = 3;
+
+        arrange(arr, arr.length);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
