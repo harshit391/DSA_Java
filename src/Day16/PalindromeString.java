019ea8b1cbc1a36007afded8a44c0e29389646e6
Harshit Singla <harshitsingla1761@gmail.com>
2024-06-25 21:53:49 +0530

    Day16

---
+++ src/Day16/PalindromeString.java
@@ -0,0 +1,32 @@
+package Day16;
+
+import java.io.*;
+
+class PalindromeString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.isPalindrome(S));
+        }
+    }
+}
+
+class Solution {
+    int isPalindrome(String S) {
+        int start = 0;
+        int end = S.length()-1;
+
+        while (start <= end) {
+            if (S.charAt(start) != S.charAt(end)) {
+                return 0;
+            }
+            start++;
+            end--;
+        }
+
+        return 1;
+    }
+}
